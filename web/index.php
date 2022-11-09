274fe3ac0a371756aa719e643865db9f4ebb4fb9
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-09 17:00:31 +0200

    Edit class Router

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ use \Core\Router;
 
 spl_autoload_register(function($class) {
     echo $path.'<br>';
-    $path = str_replace('\\', '/', "../" . $class . '.php');
+    $path = str_replace('\\', '/', $class . '.php');
     echo $path;
     if (file_exists($path)) {
         require $path;
