9f7d6bb6e938caf5b6dd3ccc6f4c813c34eeedb4
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-09 14:53:47 +0200

    Edit class Router

---
+++ core/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace core;
+
+class Router {
+    
+    function __construct() {
+        echo 'I am class Router';
+    }
+
+}
--- web/index.php
+++ web/index.php
@@ -2,8 +2,19 @@
 
 require __DIR__ . '/../lib/dev.php';
 
-debug();
+use core\Router;
 
-echo 'Hello!';
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class . '.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+$router = new Router;
+
+//debug();
+
+//echo 'Hello!';
 
 ?>
