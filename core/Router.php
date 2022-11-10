5a5300d001fc5e1ce55765e3a5805830d6c30c89
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-10 21:38:07 +0200

    Edit core/Router

--- core/Router.php
+++ core/Router.php
@@ -8,11 +8,15 @@ class Router {
 
     public function __construct() {
         $arr = require 'config/routes.php';
+        foreach ($arr as $key => $val) {
+            $this->add($key, $val);
+        }
         debug($arr);
     }
 
-    public function add() {
-        //
+    public function add($route, $params) {
+        $route = '#^' . $route . '$#';
+        $this->routes[$route] = $params;
     }
 
     public function match() {
