420fb41920682a1e41f6e7f5deeb2375c4fa2a9d
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-15 21:05:46 +0200

    Create controllers/AccountController

---
+++ controllers/AccountController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace controllers;
+
+class AccountController {
+    
+    public function loginAction() {
+        echo 'Сторінка входу';
+    }
+
+    public function registerAction() {
+        echo 'Сторінка реєстрації';
+    }
+}
+
+?>
