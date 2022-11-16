c46c77085aac1cf0c2fa5fae60b5a41b0f169f91
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-16 14:59:59 +0200

    Edit core/View

--- core/View.php
+++ core/View.php
@@ -11,10 +11,10 @@ class View {
     public function __construct($route) {
         $this->route = $route;
         $this->path = $route['controller'].'/'.$route['action'];
-        debug($this->path);
     }
 
+    public function render($title, $vars = []) {
+        require 'views/layouts/'.$this->layout.'.php';
+    }
 }
-
-
 ?>
