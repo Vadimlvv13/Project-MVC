0d2ca6e4b3cb87ecfe25a3998fe81a6ab0df1280
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-16 13:45:29 +0200

    Edit views/account/register

--- views/account/register.php
+++ views/account/register.php
@@ -1,1 +1,8 @@
-<p>Сторінка реєстрації</p>
+<h3>Реєстрація</h3>
+<form>
+    <p>Login</p>
+    <p><input type="text"></p>
+    <p>Password</p>
+    <p><input type="password"></p>
+    <p><button>Реєстрація</button></p>
+</form>
