9e12bfd4ff6fbc1f434c382797c6f93501569db6
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-16 15:26:30 +0200

    Edit core/View

--- core/View.php
+++ core/View.php
@@ -14,10 +14,14 @@ class View {
     }
 
     public function render($title, $vars = []) {
-        ob_start();
-        require 'views/'.$this->path.'.php';
-        $content = ob_get_clean();
-        require 'views/layouts/'.$this->layout.'.php';
+        if (file_exists('views/'.$this->path.'.php')) {
+            ob_start();
+            require 'views/'.$this->path.'.php';
+            $content = ob_get_clean();
+            require 'views/layouts/'.$this->layout.'.php';
+        } else {
+            echo 'ERROR: View '.$this->path.' not found!';
+        }
     }
 }
 ?>
