c4b87285de53d30d8b799cf847b9e8a40aad36ab
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-16 13:46:19 +0200

    Edit views/account/login

--- views/account/login.php
+++ views/account/login.php
@@ -1,1 +1,8 @@
-<p>Сторінка входу</p>
+<h3>Вхід</h3>
+<form>
+    <p>Login</p>
+    <p><input type="text"></p>
+    <p>Password</p>
+    <p><input type="password"></p>
+    <p><button>Вхід</button></p>
+</form>
