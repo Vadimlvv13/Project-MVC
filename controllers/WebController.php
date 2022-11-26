4b09092b397adb4e34c322ec7d395a9b9732b494
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-27 00:31:10 +0200

    Edit controllers/WebController

--- controllers/WebController.php
+++ controllers/WebController.php
@@ -10,7 +10,7 @@ class WebController extends Controller {
     public function indexAction() {
         $db = new Db;
         $this->view->render('Головна сторінка');
-        $data = $db->query('SELECT name FROM user WHERE id = 1');
+        $data = $db->column('SELECT name FROM user WHERE id = 1');
         debug($data);
     }
 
