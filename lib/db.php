a9426ef16226371a969c922cf72ee88305e05829
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-27 00:19:22 +0200

    Edit lib/db

--- lib/db.php
+++ lib/db.php
@@ -5,10 +5,29 @@ namespace lib;
 use PDO;
 
 class Db {
+
+    protected $db;
+
     public function __construct() {
         $config = require 'config/db.php';
         $this->db = new PDO('mysql:host='.$config['host'].';dbname='.$config['dbname'].'', $config['user'], $config['password']);
     }
+
+    public function query($sql) {
+        $query = $this->db->query($sql);
+        return $query;
+    }
+
+    public function row($sql) {
+        $result = $this->query($sql);
+        return $result->fetchAll();
+    }
+
+    public function column($sql) {
+        $result = $this->query($sql);
+        return $result->fetchColumn();
+    }
+
 }
 
 
