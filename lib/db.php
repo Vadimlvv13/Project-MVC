8ec770c5b6c53f7939bce76654eceffc431f0255
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-27 00:03:58 +0200

    Edit lib/db

--- lib/db.php
+++ lib/db.php
@@ -7,7 +7,7 @@ use PDO;
 class Db {
     public function __construct() {
         $config = require 'config/db.php';
-        $this->db = new PDO('mysql:host='.$config[host].';dbname='.$config[name].'', $config[user], $config[password]);
+        $this->db = new PDO('mysql:host='.$config['host'].';dbname='.$config['name'].'', $config['user'], $config['password']);
     }
 }
 
