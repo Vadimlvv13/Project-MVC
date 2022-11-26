99ff557343bc14d3bcc70f3f57fb3f7905332336
Vadim Lisovyi <lisoviy13@gmail.com>
2022-11-26 23:52:57 +0200

    Edit controllers/WebController

--- controllers/WebController.php
+++ controllers/WebController.php
@@ -3,10 +3,12 @@
 namespace controllers;
 
 use core\Controller;
+use lib\db;
 
 class WebController extends Controller {
     
     public function indexAction() {
+        $db = new Db;
         $this->view->render('Головна сторінка');
         //echo 'Домашня сторінка';
     }
